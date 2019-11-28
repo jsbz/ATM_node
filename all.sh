913243cd200227312578dd764b8337df571aad7f
jsbz <34699652+jsbz@users.noreply.github.com>
2019-11-29 00:33:04 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
-# curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-node/master/server/all.sh | bash
+# curl -SL https://raw.githubusercontent.com/jsbz/ATM_node/master/all.sh | bash
 
-curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-base/master/server/init.sh | bash
-curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-base/master/server/docker.sh | bash
-curl -SL https://raw.githubusercontent.com/Mooxe000/mooxe-docker-node/master/server/node.sh | bash
+curl -SL https://raw.githubusercontent.com/jsbz/ATM_node/master/init.sh | bash
+curl -SL https://raw.githubusercontent.com/jsbz/ATM_node/master/docker.sh | bash
+curl -SL https://raw.githubusercontent.com/jsbz/ATM_node/master/node.sh | bash
