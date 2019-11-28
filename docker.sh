264686696861bdf83b3aa76ea92bf52471991df9
jsbz <34699652+jsbz@users.noreply.github.com>
2019-11-28 23:55:17 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -21,6 +21,6 @@ curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2
 
 service docker restart
 
-curl -L https://github.com/docker/compose/releases/download/1.24.1/\
-docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.0/\
+docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
